fa2a265c4e2367382468bc402f5d51e98dca1d73
LINM04 <m.j.vd.linden@rocmondriaan.nl>
2023-02-14 12:00:44 +0100

    pagina met plaatje en link

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -8,13 +8,20 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyController extends AbstractController
 {
-    #[Route('/lucky/number')]
-    public function number(): Response
+    #[Route('/lucky/number/{max}')]
+    public function number($max): Response
     {
-        $number = random_int(0, 100);
-
-        return $this->render('bezoeker/lucky.html.twig',['number'=>$number]);
-
-
+        $number = random_int(0, $max);
+        $dagen=["maandag","dinsdag","woensdag","donderdag","vrijdag"];
+        return $this->render('bezoeker/lucky.html.twig',
+            ['number'=>$number,
+            'days'=>$dagen]);
     }
+
+    #[Route('/goedemorgen',name:'morgen')]
+    public function goedemorgenAction():Response
+    {
+        return new Response('<h1>goedemorgen</h1>');
+    }
+
 }
