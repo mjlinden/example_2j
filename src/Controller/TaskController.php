ed911e86c69b7992fc3e17906eb1269d9eb71c5f
LINM04 <m.j.vd.linden@rocmondriaan.nl>
2023-03-16 11:52:51 +0100

    tonen Task object

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -5,13 +5,14 @@ namespace App\Controller;
 use App\Entity\Task;
 use App\Form\TaskType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TaskController extends AbstractController
 {
     #[Route('/task', name: 'app_task')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         return $this->render('task/index.html.twig', [
             'controller_name' => 'TaskController',
@@ -19,12 +20,26 @@ class TaskController extends AbstractController
     }
 
     #[Route('/taskform', name: 'app_task_form')]
-    public function taskForm(): Response
+    public function taskForm(Request $request): Response
     {
         $task = new Task();
 
 
         $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $task = $form->getData();
+            dd($task);
+            // ... perform some action, such as saving the task to the database
+
+            return $this->redirectToRoute('task_success');
+        }
+
+
+
 
         return $this->renderForm('task/new.html.twig', [
             'form' => $form,
